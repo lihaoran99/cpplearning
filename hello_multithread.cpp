aca37fff9cb2310021b20d7836f0c01ea162f4e6
lihaoran99 <hollandnest@gmail.com>
2025-04-19 16:41:53 +0800

    third commit

---
+++ hello_multithread.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <thread>       // 1.
+
+void greeting() {       // 2.
+    std::cout << "Hello multithread!" << std::endl;
+    return;
+}
+
+int main() {
+    std::thread t{greeting};    // 3.
+    t.join();                   // 4.
+    return 0;
+}
